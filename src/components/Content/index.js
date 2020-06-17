6968199c0253c7ec936a6dc7c9cfd490935f3831
merunga <merunga@gmail.com>
2020-06-17 11:32:41 -0500

    extraer character card

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
 
 import data from '../../lib/data.json';
+import CharacterCard from '../CharacterCard';
 
-const Content = () => (
-  <Container fluid>
-    <Row>
-      {data.results.map((character) => (
-        <Col key={character.id} xs={12} sm={6} md={4} lg={3}>
-          <Card>
-            <Card.Img variant="top" src={character.image} />
-            <Card.Body>
-              <Card.Title>{character.name}</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-  </Container>
-);
+const Content = () => {
+  const cards = data.results.map((character) => {
+    return (
+      <CharacterCard key={character.id} character={character} />
+    )
+  });
+  
+  return (
+    <Container fluid>
+      <Row>
+        {cards}
+      </Row>
+    </Container>
+  )}
+;
 
 export default Content;
--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+// import React from 'react';
 
 const Filter = () => (
-  <div>Filter</div>
+  null
 );
 
 export default Filter;
