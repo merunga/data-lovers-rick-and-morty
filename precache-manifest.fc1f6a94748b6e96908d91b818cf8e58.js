d84ff8e832dab04d9493e0a74e058b66f8541c1d
merunga <merunga@gmail.com>
2020-06-17 11:39:30 -0500

    Updates

---
+++ precache-manifest.fc1f6a94748b6e96908d91b818cf8e58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de533d4f5acb8bf9784950481726f3c7",
+    "url": "/data-lovers-rick-and-morty/index.html"
+  },
+  {
+    "revision": "c471867d5be3ea7d68dc",
+    "url": "/data-lovers-rick-and-morty/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c67f7dc4f8122e33a873",
+    "url": "/data-lovers-rick-and-morty/static/css/main.8c869fd7.chunk.css"
+  },
+  {
+    "revision": "c471867d5be3ea7d68dc",
+    "url": "/data-lovers-rick-and-morty/static/js/2.2919b5f4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/data-lovers-rick-and-morty/static/js/2.2919b5f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67f7dc4f8122e33a873",
+    "url": "/data-lovers-rick-and-morty/static/js/main.5bfdd3f4.chunk.js"
+  },
+  {
+    "revision": "2dfac003d91b3399f28b",
+    "url": "/data-lovers-rick-and-morty/static/js/runtime-main.75bba6cc.js"
+  }
+]);
