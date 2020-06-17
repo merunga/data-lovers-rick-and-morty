0ec871ca424402e05b0e7b39bf476fd1600b4658
merunga <merunga@gmail.com>
2020-06-17 11:00:52 -0500

    Component filter contetn placeholder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 
 import Header from './components/Header';
+import Filter from './components/Filter';
+import Content from './components/Content';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,6 +11,8 @@ function App() {
   return (
     <Container>
       <Header />
+      <Filter />
+      <Content />
     </Container>
   );
 }
