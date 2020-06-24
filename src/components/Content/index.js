13ddb527c043f272d318dfa8c55e10c4598b78df
merunga <merunga@gmail.com>
2020-06-24 17:44:21 -0500

    avances preparacion sesion 2 capsula

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-import data from '../../lib/data.json';
+// import data from '../../lib/data.json';
 import CharacterCard from '../CharacterCard';
 
-const Content = () => {
+const Content = ({ searchText }) => {
+  const [data, setData] = useState({ info: {}, results: [] });
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const searchTextUrlParam = searchText.trim() ? `?name=${searchText.trim()}` : '';
+    setLoading(true);
+    fetch(`https://rickandmortyapi.com/api/character${searchTextUrlParam}`)
+      .then((response) => response.json())
+      .then((newData) => {
+        let result;
+        if (newData.error) {
+          result = {
+            info: {},
+            results: [],
+          }
+        } else {
+          result = newData;
+        }
+        setData(result)
+        setLoading(false)
+      });
+  }, [searchText])
+
+  if (loading) {
+    return 'Loading...';
+  }
+
   const cards = data.results.map((character) => {
     return (
       <CharacterCard key={character.id} character={character} />
@@ -15,6 +43,9 @@ const Content = () => {
   return (
     <Container fluid>
       <Row>
+        {data.results.length === 0 && (
+          <Col><h3>No se encontraron resultados para "{searchText}"</h3></Col>
+        )}
         {cards}
       </Row>
     </Container>
--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,7 +1,29 @@
-// import React from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
 
-const Filter = () => (
-  null
-);
+const Filter = ({ searchText, setSearchText }) => {
+  const [searchTextTemp, setSearchTextTemp] = useState(searchText);
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setSearchText(searchTextTemp);
+  }
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Group>
+        <Form.Control
+          size="lg"
+          type="text"
+          placeholder="Busca un personaje"
+          onChange={(e) => {
+            setSearchTextTemp(e.target.value);
+          }}
+          value={searchTextTemp}
+        />
+      </Form.Group>
+    </Form>
+  )
+};
 
 export default Filter;
