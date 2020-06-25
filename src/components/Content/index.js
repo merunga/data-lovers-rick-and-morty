0f59bfc30eb56e8f2df27828f146ef7ec219cc9d
Laboratoria <laboratoria@MacBook-Air-de-Laboratoria.local>
2020-06-25 00:37:15 -0500

    Adding loading to content component

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // import data from '../../lib/data.json';
 import CharacterCard from '../CharacterCard';
 
 const Content = ({ searchText }) => {
   const [data, setData] = useState({ info: {}, results: [] });
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const searchTextUrlParam = searchText.trim() ? `?name=${searchText.trim()}`: '';
+    setLoading(true);
     fetch(`https://rickandmortyapi.com/api/character${searchTextUrlParam}`)
     .then(response => response.json())
     .then(newData => {
@@ -22,9 +25,14 @@ const Content = ({ searchText }) => {
       } else {
         result = newData;
       }
-      setData(result)
+      setData(result);
+      setLoading(false);
     });
-  }, [searchText])
+  }, [searchText]);
+
+  if(loading) {
+    return 'Loading...';
+  }
 
   const cards = data.results.map((character) => {
     return (
@@ -36,6 +44,9 @@ const Content = ({ searchText }) => {
   return (
     <Container fluid>
       <Row>
+        {data.results.length === 0 && (
+          <Col><h3>No se encontraron resultados</h3></Col>
+        )}
         {cards}
       </Row>
     </Container>
