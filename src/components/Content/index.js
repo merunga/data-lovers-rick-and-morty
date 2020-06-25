846df58a10abd26af142d3001564ce79b591bb77
Laboratoria <laboratoria@MacBook-Air-de-Laboratoria.local>
2020-06-25 00:05:44 -0500

    Adding useEffect hook and fetch to content component

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-import data from '../../lib/data.json';
+// import data from '../../lib/data.json';
 import CharacterCard from '../CharacterCard';
 
 const Content = ({ searchText }) => {
+  const [data, setData] = useState({ info: {}, results: [] });
+
+  useEffect(() => {
+    const searchTextUrlParam = searchText.trim() ? `?name=${searchText.trim()}`: '';
+    fetch(`https://rickandmortyapi.com/api/character${searchTextUrlParam}`)
+    .then(response => response.json())
+    .then(newData => {
+      let result;
+      if(newData.error) {
+        result = {
+          info: {},
+          results: []
+        }
+      } else {
+        result = newData;
+      }
+      setData(result)
+    });
+  }, [searchText])
 
   const cards = data.results.map((character) => {
     return (
@@ -13,16 +32,11 @@ const Content = ({ searchText }) => {
     )
   });
 
-  const characters = cards
-    .filter((c) => !searchText || c.props.character.name.match(new RegExp(searchText, 'ig')))
-    
+ 
   return (
     <Container fluid>
       <Row>
-        {characters.length ?
-          characters:
-          cards
-        }
+        {cards}
       </Row>
     </Container>
   )}
